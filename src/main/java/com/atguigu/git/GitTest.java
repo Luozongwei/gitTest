2c0bfa9a24cfc31490ca8e297a14a9427b4ec9a6
Luozongwei <2898175301@qq.com>
2023-02-09 22:25:04 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello hot-fix");
         System.out.println("master test2");
         System.out.println("hello hot-fix test");
+        System.out.println("push test");
     }
 }
