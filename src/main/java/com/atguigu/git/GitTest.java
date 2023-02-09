3d2f39e596eac401ba46f95e34ed37db876012a2
Luozongwei <2898175301@qq.com>
2023-02-09 21:37:41 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName GitTest
+ * @Author luozongwei
+ * @Date 2023/2/9 21:32
+ * @Version 1.0
+ * @Description TODO
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
